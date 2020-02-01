d4b91e43cf42fdd43ac82894a70f249fd49f3308
Shin <ansrl0107@gmail.com>
2020-02-01 19:04:09 +0900

    report print 기능 추가

--- src/pages/report.tsx
+++ src/pages/report.tsx
@@ -2,16 +2,19 @@ import React, { FC, useState, useEffect, useCallback, CSSProperties } from 'reac
 import { TaskTable } from '../components';
 import { getTeamTasks } from '../api';
 import { Task } from '../types';
-import { DatePicker } from 'antd';
+import { DatePicker, Button, Col } from 'antd';
 import moment from 'moment';
 import 'moment-timezone';
 import { RangePickerValue } from 'antd/lib/date-picker/interface';
+import html2canvas from 'html2canvas';
+import jsPDF from 'jspdf';
 
 const Report: FC = () => {
   const now = moment().tz('Asia/Seoul');
   const initialDateRange: [moment.Moment, moment.Moment] = [moment(now).day(1), moment(now).day(5)];
   const [dateRange, setDateRange] = useState(initialDateRange);
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [printLoading, setPrintLoading] = useState(false);
   const onChangeDateRange = (dates: RangePickerValue) => {
     const [startDate, endDate] = dates;
     if (startDate && endDate) {
@@ -30,7 +33,6 @@ const Report: FC = () => {
   const style: CSSProperties = {
     display: 'block',
     margin: '0 auto',
-    maxWidth: 256,
     width: '100%',
   };
   const usernames = tasks
@@ -39,10 +41,68 @@ const Report: FC = () => {
   const renderUserTaskTable = (username: string) => (
     <TaskTable tasks={tasks.filter(task => task.user.name === username)}/>
   );
+  const printReport = async () => {
+    setPrintLoading(true);
+    const target = document.querySelector<HTMLElement>('section.report-table-list');
+    const tables = document.querySelectorAll('.ant-table-wrapper');
+    let h = 0;
+    tables.forEach(table => h += (table.clientHeight + 32));
+
+    if (target) {
+      const canvas = await html2canvas(target, {
+        height: h,
+        windowHeight: h,
+        width: tables.item(0).clientWidth + 16,
+      });
+      const imgData = canvas.toDataURL('image/png');
+
+      const imgWidth = 210;
+      const imgHeight = canvas.height * imgWidth / canvas.width;
+      const pageHeight = imgWidth * 1.414;
+      let heightLeft = imgHeight;
+
+      const doc = new jsPDF('p', 'mm');
+      let position = 0;
+
+      doc.addImage(imgData, 'PNG', 16, position, imgWidth - 32, imgHeight);
+      heightLeft -= pageHeight;
+
+      while (heightLeft >= 20) {
+        position = heightLeft - imgHeight;
+        doc.addPage();
+        doc.addImage(imgData, 'PNG', 16, position, imgWidth - 32, imgHeight);
+        heightLeft -= pageHeight;
+      }
+
+      const [from, to] = dateRange.map(d => d.format('YYYY-MM-DD'));
+      doc.save(`개발팀 주간보고 ${from}~${to}.pdf`);
+      setPrintLoading(false);
+    }
+  };
+
+  const datepickerSize = {
+    xs: { offset: 0, span: 24 },
+    sm: { offset: 0, span: 18 },
+    md: { offset: 6, span: 12 },
+    lg: { offset: 8, span: 8 },
+  };
+  const printButtonSize = {
+    xs: { offset: 0, span: 24 },
+    sm: { offset: 0, span: 6 },
+    md: { push: 2, span: 4 },
+    lg: { push: 6, span: 2 },
+  }
   return (
     <React.Fragment>
-      <DatePicker.RangePicker onChange={onChangeDateRange} value={dateRange} style={style}/>
-      {usernames.map(renderUserTaskTable)}
+      <Col {...datepickerSize}>
+        <DatePicker.RangePicker onChange={onChangeDateRange} value={dateRange} style={style}/>
+      </Col>
+      <Col {...printButtonSize}>
+        <Button loading={printLoading} onClick={printReport} block={true}>Print</Button>
+      </Col>
+      <section className="report-table-list">
+        {usernames.map(renderUserTaskTable)}
+      </section>
     </React.Fragment>
   );
 };
